b3fc802f5c5b37c82f8919b6e7f737e53f8f343c
Jakub Wlodek <jwlodek@bnl.gov>
2024-06-06 11:09:48 -0400

    More reliable reconnect

--- tscsd.py
+++ tscsd.py
@@ -170,7 +170,6 @@ class SimpleDevice:
         self._socket_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for quick restarts
         self._socket_conn.bind((self._intf, self._port))
         self._socket_conn.listen(1)
-        print("Starting simple device...")
         self._socket_conn.settimeout(5.0)
         connected = False
         while self._keep_alive and not connected:
@@ -184,7 +183,7 @@ class SimpleDevice:
                 pass
 
     def communicate(self):
-
+        print("Starting simple device...")
         client_socket = self.wait_for_conn()
         while self._keep_alive:
             try:
@@ -196,7 +195,7 @@ class SimpleDevice:
                         client_socket.sendall(str.encode(f"{output}{self._out_term}"))
             except RuntimeError as e:
                 print("Socket disconnected. Waiting for new connection...")
-                self.wait_for_conn()
+                client_socket = self.wait_for_conn()
 
     def execute_command(self, command_as_list):
         base_cmd = command_as_list[0]
